42f6ac6a03bac269cc0232ee6a097c3df9271af9
maclarenscott <maclarenscottdev@gmail.com>
2023-07-19 23:47:39 -0400

    meta description and title for seo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <title>MacLaren Scott | Software Engineer Intern at TD Securities | Mathematics and Computer Science Student at McGill</title>
+          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+          <meta
+            name="description"
+            content="MacLaren Scott, a Software Engineering Intern at TD Securities and Math & CS Student at McGill University. Specializing in Java, Jenkins, and web development. Explore my innovative projects in quantitative finance and software engineering."
+          />
         </Head>
         <body>
           <Main />
